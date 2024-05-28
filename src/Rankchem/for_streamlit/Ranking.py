5deb0677c656ac76b0fb958802f12b0f8f006cc8
fracaludo <160873846+fracaludo@users.noreply.github.com>
2024-05-28 14:13:23 +0200

    help

--- src/Rankchem/for_streamlit/Ranking.py
+++ src/Rankchem/for_streamlit/Ranking.py
@@ -86,13 +86,14 @@ def run_Ranking():
     Output:
     Displays the ranked list of molecules based on their descriptor values.
     '''
+  
     st.title("Molecule Ranking")
-    st.write("Use the options below to rank molecules in order of decreasing reacivity (Top molecule will be most reactive and bottom molecule the least reactive).")
+    st.write("This window runs the molecule ranking script.")
 
     # Input widgets for Streamlit
     user_input = st.text_input("Enter a list of SMILES strings separated by commas:", "CC=O,CC(=O)C,O=COC,CN(C)C(C)=O")
     descriptor_type = st.radio("Should we be ranking according to nucleophilicity (N) or electrophilicity (E)?", ('N', 'E'))
-    iterations = st.slider("Number of iterations (increase for a more precise result).", min_value=1, max_value=400, value=10)
+    iterations = st.slider("Number of iterations", min_value=1, max_value=100, value=10)
     
     if st.button("Run Ranking"):
         smiles_list = [smiles.strip() for smiles in user_input.split(',')]
@@ -100,7 +101,7 @@ def run_Ranking():
         ranked_descriptors = rank_descriptors(descriptors)
 
         st.write("Ranked list of molecules based on their descriptors:")
-        for smiles descriptor in ranked_descriptors:
+        for smiles, descriptor in ranked_descriptors:
             st.write(f"{smiles}: {descriptor}")
 
 if __name__ == "__main__":
