0f07df2df29f7a05c542edb136a4d549cd84cdb6
fracaludo <160873846+fracaludo@users.noreply.github.com>
2024-05-29 20:34:35 +0200

    1.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='rankchem',
-    version='1.1.4',
+    version='1.1.5',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
