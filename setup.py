3b846af8dc725484278314ec5cf352becb107f9c
kappelemma <emma.kappeler@epfl.ch>
2024-05-29 16:28:00 +0200

    update package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rankchem',
-    version='0.1.1',
+    version='1.1.1',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
